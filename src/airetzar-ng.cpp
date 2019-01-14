05569f62092a28485716d4e2d77de0aa7a7d67ff
vvbv <jeison.cardona@correounivalle.edu.co>
2019-01-14 03:02:09 -0500

    refact

--- src/airetzar-ng.cpp
+++ src/airetzar-ng.cpp
@@ -1,8 +1,26 @@
-#include "airetzar-ng.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::make_tuple;
+using std::get;
+using std::tuple;
+using std::stoi;
+using std::ifstream;
+
+string trim(const string& str);
 
 int main(int argc, const char* argv[]){
 
     string airodump_csv, interface = "";
+    int iterations = 0;
 
     bool toggle_flag = true;
     string option, param = "";
@@ -16,6 +34,8 @@ int main(int argc, const char* argv[]){
                 interface = param;
             }else if( option == "-f" ){
                 airodump_csv = param;
+            }else if( option == "-t" ){
+                iterations = stoi( param );
             }
             toggle_flag = true;
         }
@@ -23,7 +43,48 @@ int main(int argc, const char* argv[]){
 
     cout << airodump_csv << " " << interface << endl;
 
+    struct wireless_network {
+        string bssid;
+        int channel;
+        string essid;
+    };
+
+    vector < string > w_network_raw;
+
+    string line;
+    ifstream file;
+    file.open( airodump_csv.c_str() );
+    bool first = true;
+    while(getline(file, line)) {
+        line = trim(line);
+        if(first){
+            if (line.find("BSSID") != string::npos) {
+                first = false;
+            }
+        }else{
+            if( line == "" ){
+                break;
+            }else{
+                w_network_raw.push_back( line );
+            }
+        }
+    }
+    file.close();
+
+    cout << w_network_raw.size() << endl;
+
     //int x = std::system("macvendor --no-update b4:82:fe:cf:12:28");
     
     return 0;
+}
+
+string trim(const string& str){
+
+    size_t first = str.find_first_not_of(' ');
+    if (string::npos == first)
+    {
+        return str;
+    }
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
 }
